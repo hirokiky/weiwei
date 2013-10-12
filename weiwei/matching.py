8c0bc4a12e5283809f0aabac0404b1a3f17c3641
hirokiky <hirokiky@gmail.com>
2013-10-12 17:34:04 +0900

    Removed a matching for logout views

--- weiwei/matching.py
+++ weiwei/matching.py
@@ -1,10 +1,9 @@
 from matcha import Matching as m, bundle
 
 
-from weiwei.web import login_dispatch, logout_dispatch, page_dispatch
+from weiwei.web import login_dispatch, page_dispatch
 
 matching = bundle(
     m('/login', login_dispatch, name='login'),
-    m('/logout', logout_dispatch, name='logout'),
     m('/{page_title}', page_dispatch, name='page'),
 )
